575c05669843b051d72f90a08f8fb9f069a3bf7a
josef <josef.ludvicek.cz@gmail.com>
2014-09-17 22:30:39 +0200

    test_project settings use yahoo-finance
     - EXCHANGE_ADAPTER_CLASS changed to yahoo finances
     - debug true
     - setup debug logging

--- test_project/settings.py
+++ test_project/settings.py
@@ -10,3 +10,36 @@ DATABASES = {
 TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
 
 OPENEXCHANGERATES_API_KEY = '<DUMMY_KEY>'
+
+SECRET_KEY = '<DUMMY_KEY>'
+DEBUG = True
+
+EXCHANGE_ADAPTER_CLASS = 'exchange.adapters.yahoofinancerates.YahooFinanceRatesAdapter'
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': '%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s'
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        },
+    },
+    'handlers': {
+
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'simple'
+        }
+    },
+    'loggers': {
+        '': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+    },
+}
